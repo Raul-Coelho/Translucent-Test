849a803047cd90d8b5742e0f4f2e7088674a1a22
Raul Cabral <raulcc50@gmail.com>
2021-08-27 09:26:54 -0300

    Creating game model

---
+++ back-end/src/main/java/com/translucent/gamecatalog/model/Game.java
@@ -0,0 +1,38 @@
+package com.translucent.gamecatalog.model;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Game {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NotNull
+    private String title;
+    @NotNull
+    private String year;
+    @NotNull
+    private String console;
+    @NotNull
+    private Boolean completed;
+    @Column(nullable = true)
+    private LocalDate dateOfCompletion;
+    @NotNull
+    private String personalNotes;
+
+}
