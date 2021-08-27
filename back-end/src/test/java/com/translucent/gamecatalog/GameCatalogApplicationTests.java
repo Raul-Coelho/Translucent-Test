6a1ca90b9519092552bb8c6eaeaeb37d4235399e
Raul Cabral <raulcc50@gmail.com>
2021-08-27 09:13:26 -0300

    Create back-end project

---
+++ back-end/src/test/java/com/translucent/gamecatalog/GameCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translucent.gamecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
