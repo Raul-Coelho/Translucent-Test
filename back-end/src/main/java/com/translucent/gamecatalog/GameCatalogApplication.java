4742c9715752f5f2aacfefb7635b2da812714ac0
Raul Cabral <raulcc50@gmail.com>
2021-08-27 09:35:05 -0300

    Creating swagger

---
+++ back-end/src/main/java/com/translucent/gamecatalog/GameCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.translucent.gamecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCatalogApplication.class, args);
+	}
+
+}
