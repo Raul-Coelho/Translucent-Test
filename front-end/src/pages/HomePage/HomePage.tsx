23a2034e3f6e8e81162429aaf414b4e213285fe0
Raul Cabral <raulcc50@gmail.com>
2021-09-04 11:05:55 -0300

    finalized search bar with autocomplete

--- front-end/src/pages/HomePage/HomePage.tsx
+++ front-end/src/pages/HomePage/HomePage.tsx
@@ -1,23 +1,30 @@
 import {
-  Toolbar, Box, Grid,
-  CircularProgress,
-  Typography,
+  Toolbar, Grid, TextField, makeStyles, Theme,
 } from '@material-ui/core';
+import { createStyles } from '@material-ui/core/styles';
+import { Autocomplete } from '@material-ui/lab';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import GameCard from '../../components/GameCard/GameCard';
 import { Game } from '../../interfaces/GameInterface';
-import GameService from '../../services/GameService/GameService';
 import { fetchGames } from '../../store/gameCatalog/action';
 import { RootState } from '../../store/rootReducer';
 
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
 const HomePage = (): JSX.Element => {
   const dispatch = useDispatch();
+  const classes = useStyles();
 
-  const { loaded, loading } = useSelector((state: RootState) => state.game);
+  const { loaded } = useSelector((state: RootState) => state.game);
   const useGames = useSelector((state: RootState) => state.game.data);
 
   const [games, setGames] = useState<Game[]>([]);
+  const [searchText, setSearchText] = useState<string>('');
 
   useEffect(() => {
     if (!loaded) {
@@ -39,17 +46,46 @@ const HomePage = (): JSX.Element => {
     }
   }, [useGames]);
 
+  const requestSearch = (searchedVal: string | null) => {
+    if (searchedVal) {
+      const filteredRows = useGames.filter((game: any) => game.title.toLowerCase().includes(searchedVal.toLowerCase()));
+      setGames(filteredRows);
+    } else {
+      setGames(useGames);
+    }
+  };
+
   return (
-    <Box p={4}>
+    <>
       <Toolbar />
-      <Grid container spacing={9}>
+      <Grid container className={classes.root} spacing={3}>
+        <Toolbar />
+        <Grid item xs={12}>
+          <Autocomplete
+            freeSolo
+            fullWidth
+            disableListWrap
+            id="free-solo-demo"
+            value={searchText}
+            onChange={(event, newValue) => requestSearch(newValue)}
+            options={games.map((option) => option.title)}
+            size="small"
+            renderInput={(params) => (
+              <>
+                <TextField {...params} label="Search in game catalog..." margin="normal" variant="outlined" />
+
+              </>
+            )}
+          />
+        </Grid>
         {games?.map((game) => (
           <Grid key={game.id} item lg={3} md={4} sm={6} xs={12}>
             <GameCard game={game} />
           </Grid>
         ))}
       </Grid>
-    </Box>
+    </>
+
   );
 };
 
