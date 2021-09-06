db74876a5680c780ec1be13acebc58bfadaf98eb
Raul Cabral <raulcc50@gmail.com>
2021-09-05 22:32:23 -0300

    Dockerfile front

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
     'react/jsx-no-bind': 'off',
     'import/no-extraneous-dependencies': ['error', { devDependencies: false, optionalDependencies: false, peerDependencies: false }],
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
     'no-console': 'off',
     'func-names': 'off',
     'max-len': 'off',
